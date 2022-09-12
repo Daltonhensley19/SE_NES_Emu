1b5118703249987c1e25ca4570c91672115e8430
Daltonhensley19 <Daltonhensley2000@gmail.com>
2022-09-12 15:29:49 -0400

    Reorder file `types.h`

--- include/types.h
+++ include/types.h
@@ -1,4 +1,17 @@
+#pragma once
+
 #include <cstdint>
+
+// A `byte` is just a 8-bit unsigned data type.
+// A `word` is just a 16-bit unsigned data type.
+//
+// We will used these to represent registers (some are 8-bit, others 16-bit.) 
+// All memory ADDRESSES (locations in memory) should be a `word` data type.
+// NOTE: This is just a type alias for these types!
+using byte = std::uint8_t;
+using word = std::uint16_t;
+
+
 
 // `0xFF` is `0b1111_1111`, which is one byte with all ones! 
 // `0xFFFF` is `0b1111_1111_1111_1111`, which is two bytes with all ones!
@@ -9,15 +22,6 @@ constexpr byte MAX_BYTE_SIZE = 0xFF;
 constexpr word MAX_WORD_SIZE = 0xFFFF;
 constexpr byte BYTE_SHIFT = 8;
 
-
-// A `byte` is just a 8-bit unsigned data type.
-// A `word` is just a 16-bit unsigned data type.
-//
-// We will used these to represent registers (some are 8-bit, others 16-bit.) 
-// All memory ADDRESSES (locations in memory) should be a `word` data type.
-// NOTE: This is just a type alias for these types!
-using byte = std::uint8_t;
-using word = std::uint16_t;
 
 
 
