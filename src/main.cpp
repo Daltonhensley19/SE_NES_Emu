b29d52909d4abcdbb0f151bea7909717a70ded54
Daltonhensley19 <Daltonhensley2000@gmail.com>
2022-09-12 15:31:09 -0400

    add very basic argument parsing to read binary

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include "../include/memory.h"
+#include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
+  if (argc != 2)
+  {
+    std::cout << "ERROR: EXPECTED ONLY PATH TO BINARY!\n";
+  }
+  else
+  {
+    auto mem = Memory(argv[1]);
+    mem.read_contents();
+  }
 
   return 0;
 }
