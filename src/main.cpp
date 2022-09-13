cc74a0513cf008d5301c5f88e30475ddf0149503
Daltonhensley19 <Daltonhensley2000@gmail.com>
2022-09-13 10:51:08 -0400

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,17 @@ int main(int argc, char** argv)
   }
   else
   {
-    auto mem = Memory(argv[1]);
+    Memory mem = Memory(argv[1]);
     mem.read_contents();
+
+    mem.write_two_bytes((u16)'hc', 0);
+
+
+    mem.read_contents();
+
+      // Location: 1 -> 0x3 
+      // Location: 2 -> 0x1
+      // Location: 3 -> 0x2
   }
 
   return 0;
