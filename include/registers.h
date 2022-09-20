4a7fa76a24b8046efa13e6a53949618cc97fbf5a
Daltonhensley19 <Daltonhensley2000@gmail.com>
2022-09-19 23:08:35 -0400

    Add registers to emulator

--- include/registers.h
+++ include/registers.h
@@ -2,9 +2,23 @@
 
 #include "types.h"
 
-struct Registers 
+class Registers
 {
-    u16 pc;
-    u16 sp;
-    u8 acc;
+  u16 pc;
+  u16 sp;
+  u16 ix;
+  u16 iy;
+
+  u16 AF1;
+  u16 BC1;
+  u16 DE1;
+  u16 HL1;
+
+  u16 AF2;
+  u16 BC2;
+  u16 DE2;
+  u16 HL2;
+
+  u8 I;
+  u8 R;
 };
