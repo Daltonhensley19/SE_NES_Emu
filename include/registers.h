1c2e88cc985d5b4602b6f5c2413db4364e54ab8a
Daltonhensley19 <Daltonhensley2000@gmail.com>
2022-09-21 13:25:07 -0400

    Better model general registers and their shadows

--- include/registers.h
+++ include/registers.h
@@ -1,24 +1,32 @@
 #pragma once
 
+#include "flags.h"
 #include "types.h"
-
 class Registers
 {
-  private:
+private:
   u16 pc;
   u16 sp;
   u16 ix;
   u16 iy;
 
-  u16 AF1;
-  u16 BC1;
-  u16 DE1;
-  u16 HL1;
+  u8 A;
+  Flags F;
+  u8 B;
+  u8 C;
+  u8 D;
+  u8 E;
+  u8 H;
+  u8 L;
 
-  u16 AF2;
-  u16 BC2;
-  u16 DE2;
-  u16 HL2;
+  u8 A_SHADOW;
+  Flags F_SHADOW;
+  u8 B_SHADOW;
+  u8 C_SHADOW;
+  u8 D_SHADOW;
+  u8 E_SHADOW;
+  u8 H_SHADOW;
+  u8 L_SHADOW;
 
   u8 I;
   u8 R;
