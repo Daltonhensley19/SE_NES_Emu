f293e1a74abf441cb3699036287154031db06d18
Daltonhensley19 <Daltonhensley2000@gmail.com>
2022-09-29 23:44:54 -0400

    Begin work on Flag helper methods

--- include/flags.h
+++ include/flags.h
@@ -9,4 +9,25 @@ private:
 
 public:
   // TODO: Helper methods to check, set, or clear bits go here
+
+  bool is_c_flag_set() const;
+  bool is_n_flag_set() const;
+  bool is_pv_flag_set() const;
+  bool is_h_flag_set() const;
+  bool is_z_flag_set() const;
+  bool is_s_flag_set() const;
+
+  void set_c_flag();
+  void set_n_flag();
+  void set_pv_flag();
+  void set_h_flag();
+  void set_z_flag();
+  void set_s_flag(); 
+  
+  void clear_c_flag();
+  void clear_n_flag();
+  void clear_pv_flag();
+  void clear_h_flag();
+  void clear_z_flag();
+  void clear_s_flag();
 };
